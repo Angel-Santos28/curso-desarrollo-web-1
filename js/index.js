2fbc1b0b7b63e9f0ab5c40ca736a070163b62658
Angel <angel.anselmo2801@gmail.com>
2022-12-14 23:12:12 -0600

    Introducción al lenguaje Javascript

---
+++ js/index.js
@@ -0,0 +1,51 @@
+//Actividad 1:
+
+//TIPOS DE VARIABLES
+
+//String
+let usuario = "Angel";
+console.log(usuario);
+
+//Number
+let edad = "22";
+console.log(edad);
+
+//Boolean
+let verdadero = true;
+let falso = false;
+console.log(verdadero);
+console.log(falso);
+
+//Undefined
+let indefinido = undefined;
+console.log(indefinido);
+
+//Null
+let nulo = null;
+console.log(nulo);
+
+
+//Actividad 2:
+//Crear 2 variables y concatenar con interpolación de texto
+let nombre = "Angel";
+let apellido = "Anselmo";
+
+console.log(`Mi nombre es ${nombre} y mi apellido paterno es ${apellido}`);
+
+
+/*Actividad 3:
+Crear 2 variables tipo Number y realizar: */
+let a = 3;
+let b = 4;
+
+//Operación aritmética
+ console.log(a + b);
+
+//Asignaciones + operación aritmética
+console.log(a = b);
+console.log(a += b);
+console.log(a + b);
+
+//Comparación
+console.log(a == b);
+console.log(a != b);
